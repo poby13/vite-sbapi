371ccaff402935870d0723dee168276a560cdf3f
shinis(ys) <actionshin@gmail.com>
2024-11-28 11:20:45 +0900

    주소창의 파라미터 추출 useParams()

--- src/router/todoRouter.jsx
+++ src/router/todoRouter.jsx
@@ -1,25 +1,34 @@
-import {Suspense, lazy} from "react";
+import { Suspense, lazy } from "react";
 import { Navigate } from "react-router-dom";
 
 const Loading = <div>Loading....</div>;
 const TodoList = lazy(() => import("../pages/todo/ListPage.jsx"));
+const TodoRead = lazy(() => import("../pages/todo/ReadPage.jsx"));
 
 const todoRouter = () => {
-  return [
-    {
-      path: "list",
-      element: (
-        <Suspense fallback={Loading}>
-          <TodoList></TodoList>
-        </Suspense>
-      ),
-    },
-    {
-        /* `/todo/` 이하의 경로가 지정되지 않았을 경우 동작 <= 리다이렉션 처리 */
-        path: "",
-        element: <Navigate replace to="list"></Navigate>
-    }
-  ];
+    return [
+        {
+            path: "list",
+            element: (
+                <Suspense fallback={Loading}>
+                    <TodoList></TodoList>
+                </Suspense>
+            ),
+        },
+        {
+            /* `/todo/` 이하의 경로가 지정되지 않았을 경우 동작 <= 리다이렉션 처리 */
+            path: "",
+            element: <Navigate replace to="list"></Navigate>,
+        },
+        {
+            path: "read/:tno",
+            element: (
+                <Suspense fallback={Loading}>
+                    <TodoRead></TodoRead>
+                </Suspense>
+            ),
+        },
+    ];
 };
 
 export default todoRouter;
